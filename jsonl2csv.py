8da1fbfb245b74cbd5e2a125078fffd9258d9b65
aga3134 <aga3134@gmail.com>
2023-10-09 13:45:43 +0800

    add argparse & requirements.txt

--- jsonl2csv.py
+++ jsonl2csv.py
@@ -1,11 +1,26 @@
 from datasets import load_dataset
 import pandas as pd
 from tqdm import tqdm
+import argparse
 
-dataset = load_dataset("json", data_files=["Traditional_dolly_conv.jsonl"], split="train")
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-i", help="輸入檔名(jsonl)", dest="input")
+  parser.add_argument("-o", help="輸出檔名(csv)", dest="output")
+  args = parser.parse_args()
 
-df = pd.DataFrame(columns=dataset.features)
-for d in tqdm(dataset):
-  df.loc[len(df)] = d
-print(df)
-df.to_csv("dolly-15k-CKIP.csv",index=False)
+  if args.input is None:
+    parser.print_help()
+    exit()
+
+  if args.output is None:
+    args.output = "output.csv"
+
+  dataset = load_dataset("json", data_files=[args.input], split="train")
+
+  df = pd.DataFrame(columns=dataset.features)
+  for d in tqdm(dataset):
+    df.loc[len(df)] = d
+
+  print(df)
+  df.to_csv(args.output,index=False)
